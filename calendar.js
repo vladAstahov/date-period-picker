d693e63daad1e0c1d7235b6e90a827ef0141c122
Vlad Astahov <avl@burno.tech>
2023-11-29 01:13:11 +0300

    start calendar

---
+++ calendar.js
@@ -0,0 +1,19 @@
+/**
+ * @typedef {Object} Day
+ * @param {number} id
+ * @param {'default' | 'active' | 'selected'}
+ */
+
+class Calendar {
+    /**
+     * @type {Day[]}
+     */
+    #list = []
+
+    /**
+     * @param {string} id
+     */
+    constructor(id) {
+
+    }
+}
